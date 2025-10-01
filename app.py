7ae71c2716bc79e2e4b885e8ea1a841df66a3ebf
christianortega83 <christian.ortega@strata52.com>
2025-10-01 12:01:12 -0600

    Staging: nueva versión de app.py y ml_quickstart.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Versión 2 - Hola desde FastAPI, funciona correcto!!!"}
+    return {"message": "Staging - probando nueva versión y ver si funciona"}
 
 @app.post("/predict")
 def predict(data: Features):
